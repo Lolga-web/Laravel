f383f8816a80d305bca93d3f3f2e0b249d995958
Lolga-web <lolga.web@mail.ru>
2021-02-21 21:40:25 +1000

    lesson1

---
+++ laravel8/resources/views/about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laravel-8</title>
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div class="container">
+        <header class="header">
+            <p class="header_title">Laravel-8</p>
+            <nav class="header_nav">
+                <ul class="header_nav_list">
+                    <li class="header_nav_item"><a href="/" class="header_nav_link">Главная</a></li>
+                    <li class="header_nav_item"><a href="/gallery" class="header_nav_link">Галерея</a></li>
+                    <li class="header_nav_item"><a href="/about" class="header_nav_link">О нас</a></li>
+                    <li class="header_nav_item"><a href="/blog" class="header_nav_link">Блог</a></li>
+                    <li class="header_nav_item"><a href="/contacts" class="header_nav_link">Контакты</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main class="main">О нас</main>
+    </div>
+</body>
+</html>
